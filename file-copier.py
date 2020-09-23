f7ffd079644cb950f160db0eb560b694af47cf8f
Rijk-van-Putten <rijkvanputten@hotmail.nl>
2020-09-23 19:29:44 +0200

    Add ignore hidden files option

--- file-copier.py
+++ file-copier.py
@@ -4,16 +4,20 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--source", "-s", help="set the source folder", required=True)
+    "--source", "-s", help="Set the source folder", required=True)
 parser.add_argument("--destination", "-d",
-                    help="set the destination folder", required=True)
+                    help="Set the destination folder", required=True)
+parser.add_argument("--hidden_files", "-hf",
+                    help="Detect hidden files", action='store_true')
 
 args = parser.parse_args()
 
 source_folder = args.source
 destination_folder = args.destination
 
-print("Seaching for new files in '{}' wich aren't in '{}'..\n".format(
+ignore_hidden = not args.hidden_files
+
+print("Seaching for new files in '{}' wich aren't already in '{}'..\n".format(
     source_folder, destination_folder))
 
 
@@ -34,22 +38,32 @@ destination_files = []
 new_files = []
 
 for root, dirs, files in os.walk(source_folder):
+    if ignore_hidden:
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+
     subdir = str(root)[len(source_folder):] + os.sep
     for name in files:
         source_files.append(FileInfo(subdir, name))
 
 for root, dirs, files in os.walk(destination_folder):
+    if ignore_hidden:
+        files = [f for f in files if not f[0] == '.']
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+
     subdir = str(root)[len(destination_folder):] + os.sep
     for name in files:
         destination_files.append(FileInfo(subdir, name))
+
 for file in source_files:
     if file not in destination_files:
         print("{} is new!".format(file.get_relative()))
         new_files.append(file)
+
 if (len(new_files) == 0):
     print("Already up-to-date! No new files found!")
 else:
-    print("\nCopying {} new files..".format(len(new_files)))
+    print("\nCopying {} new file(s)..".format(len(new_files)))
     for file in new_files:
         source = source_folder + file.get_relative()
         destination = destination_folder + file.get_relative()
