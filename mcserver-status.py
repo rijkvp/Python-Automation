28e3221211720fa7e61ae0b891ef4b2ff2dcee92
rijkvp <rijkvanputten@hotmail.nl>
2020-12-31 16:13:35 +0100

    Minecraft server status rewrite

--- mcserver-status.py
+++ mcserver-status.py
@@ -4,15 +4,13 @@ from plyer import notification
 import json
 import notifier
 
-sync_interval = 30
+sync_interval = 10
 
-with open('config/settings.json') as settings_file:
-    settings_json = json.load(settings_file)
-    sync_interval = int(settings_json["sync_interval"])
 
 class ServerInfo:
-    def __init__(self, ip):
+    def __init__(self, ip, name):
         self.ip = ip
+        self.name = name
         self.is_online = False
         self.player_count = 0
 
@@ -21,55 +19,90 @@ servers = []
 
 with open('config/mc_servers.json') as config_file:
     servers_json = json.load(config_file)
-    for server_ip in servers_json:
-        servers.append(ServerInfo(server_ip))
+    for server_name, server_ip in servers_json.items():
+        servers.append(ServerInfo(server_ip, server_name))
 
-
-def send_notification(title, fields):
-    notification = notifier.Notification(title, [notifier.NotificationCard(title, "-", fields)])
-    notifier.notify(notification, "Minecraft")
 
 def ping_servers():
     for server_info in servers:
         server = MinecraftServer.lookup(server_info.ip)
+        status = None
         try:
             status = server.status()
         except:
-            if server_info.is_online:
-                notification_fields = {
-                    "IP": server_info.ip,
-                    "Status": "Offline"
-                }
-                send_notification(server_info.ip + " is offline", notification_fields)
-                server_info.is_online = False
-            continue
+            server_info.is_online = False
 
-        player_count = status.players.online
-        ping = int(round(status.latency))
+        if status is not None:
+            player_count = status.players.online
+            max_players = status.players.max
+            ping = int(round(status.latency))
+            player_info = "{}/{}".format(player_count, max_players)
+            player_names = None
+            if status.players.sample is not None:
+                player_names = [s.name for s in status.players.sample]
+            if player_names is not None and len(player_names) == 0:
+                player_names = None
 
-        if not server_info.is_online:
+            server_version = status.version.name
+
             notification_fields = {
                 "IP": server_info.ip,
                 "Status": "Online",
-                "Players": server_info.player_count,
-                "Ping": ping
+                "Online": player_info,
+                "Ping": str(ping) + "ms",
+                "Versie": server_version
             }
-            send_notification(server_info.ip + " is online", notification_fields)
+            if player_names is not None:
+                notification_fields["Spelers"] = ", ".join(player_names)
+
+            if not server_info.is_online and player_count > 0:
+                if player_count == 1:
+                    if player_names is not None:
+                        title = "{} is online met {}".format(
+                            server_info.name, ", ".join(player_names))
+                    else:
+                        title = "{} is online met {} speler!".format(
+                            server_info.name, player_count)
+                else:
+                    title = "{} is online met {} spelers!".format(
+                        server_info.name, player_count)
+                short_desc = "{}ms - {} - {}".format(
+                    ping, server_info.ip, server_version)
+                if player_names is not None:
+                    short_desc += " - " + ",".join(player_names)
+                notifier.notify(notifier.Notification(title, [notifier.NotificationCard(
+                    "Info", "", notification_fields)], title, short_desc), "Minecraft")
+
+            if player_count != server_info.player_count and server_info.is_online:
+                diff = player_count - server_info.player_count
+
+                if diff != 0:
+                    short_desc = "{} -  {}ms - {}".format(player_info,
+                                                                                      ping, server_info.ip, server_version)
+                    if player_names is not None:
+                        short_desc += " - " + ",".join(player_names)
+                    title = None
+                    if player_count == 0:
+                        title = "Niemand meer online op {} :(".format(
+                            server_info.name)
+                    elif diff > 0:
+                        if diff == 1:
+                            title = "{} speler is {} gejoined!".format(
+                                abs(diff), server_info.name)
+                        else:
+                            title = "{} spelers zijn {} gejoined!".format(
+                                abs(diff), server_info.name)
+                    elif diff < 0:
+                        if diff == -1:
+                            title = "{} speler is {} geleaved!".format(
+                                abs(diff), server_info.name)
+                        else:
+                            title = "{} spelers zijn {} geleaved!".format(
+                                abs(diff), server_info.name)
+
+                    notifier.notify(notifier.Notification(title, [notifier.NotificationCard(
+                        "Info", "", notification_fields)], title, short_desc), "Minecraft")
             server_info.is_online = True
-            server_info.player_count = player_count
-
-        if player_count != server_info.player_count:
-            diff = player_count - server_info.player_count
-            notification_fields = {
-                "IP": server_info.ip,
-                "Status": "Online",
-                "Players": server_info.player_count,
-                "Ping": ping
-            }
-            if diff > 0:
-                send_notification("Player(s) joined", notification_fields)
-            elif diff < 0:
-                send_notification("Player(s) left", notification_fields)
             server_info.player_count = player_count
 
 
